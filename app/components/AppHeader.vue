5bdf4ad816de1dba8d42ca6d95a1735b59c2e67c
DaraCOOKED <dara.somnag020@gmail.com>
2026-02-25 15:29:01 +0700

    fix: wrap Sign Up and LogIn text in router-link components

--- app/components/AppHeader.vue
+++ app/components/AppHeader.vue
@@ -4,10 +4,12 @@
             <img src="/images/logo1.png" alt="Logo" class=" w-40">
             <div class="flex items-center space-x-4">
                 <button class="bg-[rgb(20,64,184)] hover:bg-slate-300 text-gray-700 font-semibold py-2 px-4 rounded">
-                    <h1 class="text-white text-1xl hover:text-[#000001] font-semibold">Sign Up</h1>
+                    <h1 class="text-white text-1xl hover:text-[#000001] font-semibold">
+                      <router-link to="/sign-up">Sign Up</router-link>
+                    </h1>
                     </button>
                     <button class=" border border-gray-300 bg-gray-200 hover:bg-slate-300 text-gray-700 font-semibold py-2 px-4 rounded">
-                    <h1 class="text-gray-700 text-1xl hover:text-[#000001] font-semibold">LogIn</h1>
+                    <h1 class="text-gray-700 text-1xl hover:text-[#000001] font-semibold"><router-link to="/login">LogIn</router-link></h1>
                 </button>
             </div>
             

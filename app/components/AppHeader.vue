bc3007dfb62d9a5e899a171e92c403da044a16ee
limnimoldev-netizen <git config --global user.email limnimol.dev@gmail.com>
2026-02-27 05:02:12 +0700

    change feat/header

--- app/components/AppHeader.vue
+++ app/components/AppHeader.vue
@@ -22,23 +22,18 @@
                   List Jobs
                   <span class="absolute left-0 -bottom-1 w-0 h-0.5 bg-black transition-all duration-300 group-hover:w-full"></span>
                 </a>
-                <a href="#" class="relative text-xl group">
-                  Jobs Alert
-                  <span class="absolute left-0 -bottom-1 w-0 h-0.5 bg-black transition-all duration-300 group-hover:w-full"></span>
-                </a>
-                <a href="#" class="relative text-xl group">
-                  Recommended Jobs
-                  <span class="absolute left-0 -bottom-1 w-0 h-0.5 bg-black transition-all duration-300 group-hover:w-full"></span>
-                </a>
+                
+                
                 <a href="#" class="relative text-xl group">
                   All Companies
                   <span class="absolute left-0 -bottom-1 w-0 h-0.5 bg-black transition-all duration-300 group-hover:w-full"></span>
                 </a>
             
                 <a href="#" class="relative text-xl group">
-                  Dashboard
+                  About Us
                   <span class="absolute left-0 -bottom-1 w-0 h-0.5 bg-black transition-all duration-300 group-hover:w-full"></span>
                 </a>
+
             
             </nav>
          </div>
